04d3773f11f2bded05e3b0086a4c31805b8e345e
saiemgilani <saiem.gilani@gmail.com>
2023-04-06 04:42:17 -0400

    pkg_function parameter added

--- R/upload.R
+++ R/upload.R
@@ -2,18 +2,20 @@
 #'
 #' @param files vector of filepaths to upload
 #' @param tag release name
+#' @param pkg_function related package function name
 #' @param ... other args passed to `piggyback::pb_upload()`
 #'
 #' @export
-sportsdataverse_upload <- function(files, tag, ...){
+sportsdataverse_upload <- function(files, tag, pkg_function = NULL, ...) {
   cli::cli_alert("Uploading {length(files)} files!")
   # upload files
   piggyback::pb_upload(files, repo = "sportsdataverse/sportsdataverse-data", tag = tag, ...)
   update_release_timestamp(tag)
+  update_package_function(tag, pkg_function)
   cli::cli_alert("Uploaded {length(files)} to sportsdataverse/sportsdataverse-data @ {tag} on {Sys.time()}")
 }
 
-update_release_timestamp <- function(tag){
+update_release_timestamp <- function(tag) {
   temp_dir <- tempdir(check = TRUE)
 
   update_time <- format(Sys.time(), tz = "America/Toronto", usetz = TRUE)
@@ -21,13 +23,33 @@ update_release_timestamp <- function(tag){
 
   list(last_updated = update_time) |>
     jsonlite::toJSON(auto_unbox = TRUE) |>
-    writeLines(file.path(temp_dir,"timestamp.json"))
+    writeLines(file.path(temp_dir, "timestamp.json"))
 
   piggyback::pb_upload(file.path(temp_dir,"timestamp.txt"), repo = "sportsdataverse/sportsdataverse-data", tag = tag, overwrite = TRUE)
   piggyback::pb_upload(file.path(temp_dir,"timestamp.json"), repo = "sportsdataverse/sportsdataverse-data", tag = tag, overwrite = TRUE)
 
   invisible(NULL)
 }
+
+
+update_package_function <- function(tag, pkg_function) {
+  if (!is.null(pkg_function)) {
+    temp_dir <- tempdir(check = TRUE)
+
+    package_function <- paste0(pkg_function)
+    writeLines(package_function, file.path(temp_dir, "package_function.txt"))
+
+    list(package_function = package_function) |>
+      jsonlite::toJSON(auto_unbox = TRUE) |>
+      writeLines(file.path(temp_dir, "package_function.json"))
+
+    piggyback::pb_upload(file.path(temp_dir, "package_function.txt"), repo = "sportsdataverse/sportsdataverse-data", tag = tag, overwrite = TRUE)
+    piggyback::pb_upload(file.path(temp_dir, "package_function.json"), repo = "sportsdataverse/sportsdataverse-data", tag = tag, overwrite = TRUE)
+
+    invisible(NULL)
+  }
+}
+
 
 #' Save files to sportsdataverse release
 #'
@@ -48,8 +70,8 @@ sportsdataverse_save <- function(data_frame,
                           sportsdataverse_type,
                           release_tag,
                           .token = gh::gh_token(),
-                          file_types = c("rds","csv","parquet","qs")
-){
+                          file_types = c("rds", "csv", "parquet")
+) {
 
   stopifnot(
     is.data.frame(data_frame),
@@ -65,33 +87,33 @@ sportsdataverse_save <- function(data_frame,
     length(file_types) >= 1
   )
 
-  if("season" %in% names(data_frame)) data_frame$season <- as.integer(data_frame$season)
-  if("week" %in% names(data_frame)) data_frame$week <- as.integer(data_frame$week)
+  if ("season" %in% names(data_frame)) data_frame$season <- as.integer(data_frame$season)
+  if ("week" %in% names(data_frame)) data_frame$week <- as.integer(data_frame$week)
 
-  attr(data_frame,"sportsdataverse_type") <- sportsdataverse_type
-  attr(data_frame,"sportsdataverse_timestamp") <- Sys.time()
+  attr(data_frame, "sportsdataverse_type") <- sportsdataverse_type
+  attr(data_frame, "sportsdataverse_timestamp") <- Sys.time()
 
   temp_dir <- tempdir(check = TRUE)
   ft <- rlang::arg_match(file_types,
-                         values = c("rds","csv","csv.gz","parquet","qs"),
+                         values = c("rds", "csv", "csv.gz", "parquet", "qs"),
                          multiple = TRUE)
 
-  if("rds" %in% ft) saveRDS(data_frame,file.path(temp_dir,paste0(file_name,".rds")))
-  if("csv" %in% ft) data.table::fwrite(data_frame, file.path(temp_dir,paste0(file_name,".csv")))
-  if("csv.gz" %in% ft) data.table::fwrite(data_frame, file.path(temp_dir,paste0(file_name,".csv.gz")))
-  if("parquet" %in% ft) arrow::write_parquet(data_frame, file.path(temp_dir, paste0(file_name,".parquet")))
-  if("qs" %in% ft){
+  if ("rds" %in% ft) saveRDS(data_frame, file.path(temp_dir, paste0(file_name, ".rds")))
+  if ("csv" %in% ft) data.table::fwrite(data_frame, file.path(temp_dir, paste0(file_name, ".csv")))
+  if ("csv.gz" %in% ft) data.table::fwrite(data_frame, file.path(temp_dir, paste0(file_name, ".csv.gz")))
+  if ("parquet" %in% ft) arrow::write_parquet(data_frame, file.path(temp_dir, paste0(file_name, ".parquet")))
+  if ("qs" %in% ft) {
     qs::qsave(data_frame,
-              file.path(temp_dir,paste0(file_name,".qs")),
+              file.path(temp_dir, paste0(file_name, ".qs")),
               preset = "custom",
               algorithm = "zstd_stream",
               compress_level = 22,
               shuffle_control = 15)
   }
 
-  .filetypes <- paste0(".",ft)
+  .filetypes <- paste0(".", ft)
 
-  .file_names <- file.path(temp_dir, paste0(file_name,.filetypes))
+  .file_names <- file.path(temp_dir, paste0(file_name, .filetypes))
 
-  sportsdataverse_upload(.file_names,tag = release_tag, .token = .token)
+  sportsdataverse_upload(.file_names, tag = release_tag, .token = .token)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(lib, pkg) {
-  if (Sys.getenv("SPORTSDATAVERSE.UPLOAD.INSIST", "true") == "true"){
+  if (Sys.getenv("SPORTSDATAVERSE.UPLOAD.INSIST", "true") == "true") {
     pause_base <- Sys.getenv("SPORTSDATAVERSE.UPLOAD.PAUSE_BASE", 0.05) |> as.numeric()
     pause_min <- Sys.getenv("SPORTSDATAVERSE.UPLOAD.PASUE_MIN", 1) |> as.numeric()
     max_times <- Sys.getenv("SPORTSDATAVERSE.UPLOAD.MAX_TIMES", 10) |> as.numeric()
